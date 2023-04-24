45c67cb8ca184869b4c73e27db9ae10bd4b5015e
lcupic0 <lcupic123@gmail.com>
2023-04-24 23:14:11 +0200

    Navigation DONE

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,19 +1,26 @@
+import { useState } from 'react'
 import style from './navigation.module.css'
 
 function Navigation() {
+
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+    console.log(isNavExpanded);
+
   return (
     <>
         <div className={style.wrapper}>
             <nav>
-                <img src="" alt="NAV.SLIKA" className={style.logo}/>
-                <ul className={style.list}>
-                    <li>O nama</li>
-                    <li>Popis</li>
-                    <li>Donacije</li>
-                    <li>Obavijesti</li>
-                    <li>Unos</li>
-                </ul>
-                <div className={style.mobile}>
+                <img src="/images/paw2.png" alt="Azil Šapa" className={style.logo}/>
+                <div className={`${style.listwrapper} ${isNavExpanded ? `${style.visible}` : ""}`}>
+                    <ul className={style.list}>
+                        <li>O nama</li>
+                        <li>Popis</li>
+                        <li>Donacije</li>
+                        <li>Obavijesti</li>
+                        <li>Unos</li>
+                    </ul>
+                </div>
+                <div className={`${style.mobile} ${isNavExpanded ? `${style.change}` : ''}`} onClick={() => {setIsNavExpanded(!isNavExpanded)}}>
                     <div className={style.bar1}></div>
                     <div className={style.bar2}></div>
                     <div className={style.bar3}></div>
